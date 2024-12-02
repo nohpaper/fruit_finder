f4bb6d367e884b7ce9fd1f6169607e1fca056dc7
nohpaper <nohpaper99@naver.com>
2024-12-02 19:21:17 +0900

    feat : state 변경에 따른 로직 수정

--- src/Finder.js
+++ src/Finder.js
@@ -30,94 +30,122 @@ const setps = [
 ];
 
 export default function Findex(){
-  const [fruit] = useState([
-    {name:"딸기", season:"spring", origin:"domestic", kcal:27},
-    {name:"체리", season:"spring", origin:"imports", kcal:60},
-    {name:"키위", season:"spring", origin:"domestic", kcal:55},
-    {name:"파인애플", season:"spring", origin:"imports", kcal:55},
-    {name:"오렌지", season:"spring", origin:"domestic", kcal:47},
-    {name:"수박", season:"summer", origin:"domestic", kcal:31},
-    {name:"복숭아", season:"summer", origin:"domestic", kcal:34},
-    {name:"자두", season:"summer", origin:"domestic", kcal:34},
-    {name:"망고", season:"summer", origin:"imports", kcal:68},
-    {name:"블루베리", season:"summer", origin:"imports", kcal:56},
-    {name:"사과", season:"autumn", origin:"domestic", kcal:57},
-    {name:"배", season:"autumn", origin:"domestic", kcal:51},
-    {name:"감", season:"autumn", origin:"domestic", kcal:54},
-    {name:"포도", season:"autumn", origin:"domestic", kcal:58},
-    {name:"귤", season:"winter", origin:"domestic", kcal:46},
-    {name:"대추", season:"winter", origin:"domestic", kcal:104},
-    {name:"자몽", season:"winter", origin:"imports", kcal:35},
-    {name:"레몬", season:"winter", origin:"imports", kcal:45},
-    {name:"메론", season:"summer", origin:"imports", kcal:35},
-    {name:"바나나", season:["spring", "summer", "autumn", "winter"], origin:"imports", kcal:80},
-  ]);
-  const [setp] = useState([
-    {name:"계절을 선택하세요", select:{id:"season", kind:[{korean:"봄", english:"spring"}, {korean:"여름", english:"summer"}, {korean:"가을", english:"autumn"}, {korean:"겨울", english:"winter"}]}},
-    {name:"수입 or 국산을 선택하세요", select:{id:"origin", kind:[{korean:"국산", english:"domestic"}, {korean:"수입", english:"imports"}]}},
-    {name:"칼로리를 선택하세요", select:{id:"kcal", kind:[{korean:"40미만", english:"less_than40"}, {korean:"40이상 60미만", english:"more_than40_and_less_than_60"}, {korean:"60이상", english:"more_than_60"}]}},
-  ]);
-
-  const [filtered,setFileterd] = useState([...fruits]);
-  const [selected, setSelected] = useState({
+  const [filtered,setFiltered] = useState([...fruits]);
+/*  const [selected, setSelected] = useState({
     season:[],
     origin:[],
     kcal:[],
-  });
+  });*/
+  const [selected, setSelected] = useState([
+    {
+      order:undefined,
+      id:"season",
+      kind:[],
+    },
+    {
+      order:undefined,
+      id:"origin",
+      kind:[],
+    },
+    {
+      order:undefined,
+      id:"kcal",
+      kind:[],
+    },
+  ]);
+  
+  useEffect(()=>{
+    //페이지 진입 시 fruits.kcal 부분 계산하여 filtered.kcal에 값 변경
+    fruits.map(function(element, index){
+      if(element.kcal < 40){
+        return filtered[index].kcal = "less_than40";
+      } else if(element.kcal >= 40 && element.kcal <= 60 ){
+        return filtered[index].kcal = "more_than40_and_less_than_60";
+      } else if(element.kcal > 60) {
+        return filtered[index].kcal = "more_than_60";
+      }
+      return filtered;
+    });
+    
+  }, []);
+  
   useEffect(() => {
     /* TODO::
-    *   1. kcal 일 경우 계산 필요
-    *   2. selected 내에 있는 값을 이용하여 filter 걸어서 p 태그에 노출
+    *   1. 바나나같이 여러 계절이 있을 경우
+    *   2. filter 걸기
     *
     *  */
-/*    setFileterd(()=>{
-      return fruits.filter((22)=>{22. === prev.})
-    });*/
 
-
-    /*
-    * 1. 원본(fruits)은 건들지 말것.
-    * 2. array.contains // include
-    * */
-
-    /*  selected 에 내용이 있다면
-    *   ㅇ
-    *
-    * */
+    //console.log(Array.isArray(fruits[19].season)) 배열인지 확인하는 메서드
+    
+    //첫 선택이 season 일때 (origin, kcal은 0개 일 때)
+/*    if(selected.season.length !== 0 && selected.origin.length === 0 && selected.kcal.length === 0){
+      selected.season.map(function(element){
+        filtered.filter((fruits) => fruits.season === element);
+      });
+    }
+    
+    //origin
+    if(selected.origin.length !== 0){
+      selected.origin.map(function(element){
+        console.log(element, filtered.filter((fruits) => fruits.origin === element));
+      });
+    }
+    
+    //kcal
+    if(selected.kcal.length !== 0){
+      selected.kcal.map(function(element){
+        console.log(element, filtered.filter((fruits) => fruits.kcal === element));
+      });
+    }*/
+    
+    //console.log(filtered);
 
   }, [selected]);
 
   return (<div>
       <h1>내가 원하는 과일은?</h1>
-      {setps.map(function(element){
-        return(<div>
+      {setps.map(function(element, index){
+        return(<div key={index}>
           <p>{element.name}</p>
-          {element.select.kind.map(function(child){
-            return (<div>
+          {element.select.kind.map(function(child, childIndex){
+            return (<div key={childIndex}>
               <input type="checkbox" name={child.english} id={child.english} value={child.english} onClick={(event)=>{
                 //클릭 true면 selected에 값 추가
                 const targetValue = event.target.value;
                 const clickId = element.select.id;
-                const clickCategory = selected[clickId];
-                //const clickCategory = Object.entries(selected).find((filterEle)=>filterEle[0] === clickId); //이걸로 사용할 경우 clickCategory[1]
-
-                if(!clickCategory.includes(targetValue)){
-                  //clickCategory 에 없을 때
-                  clickCategory.push(targetValue);
-                  console.log(clickCategory, selected);
+                //const clickCategory = selected[clickId];
+                const clickCategory = selected.find((filterEle)=>filterEle.id === clickId);
+                
+                //console.log(clickCategory.kind.includes(targetValue), clickCategory)
+                
+                if(!clickCategory.kind.includes(targetValue)){
+                  //clickCategory.kind 에 없을 때
+                  clickCategory.kind.push(targetValue);
                 }else {
-                  //clickCategory 에 있을 때
-                  const findIndex = clickCategory.indexOf(targetValue); //target.value index 확인
-                  clickCategory.splice(findIndex, 1); //삭제
+                  //clickCategory.kind 에 있을 때
+                  const findIndex = clickCategory.kind.indexOf(targetValue); //target.value index 확인
+                  clickCategory.kind.splice(findIndex, 1); //삭제
                 }
-
-                setSelected((prev)=> ({...selected, [clickId]: clickCategory}));
+                //console.log("result",clickCategory.kind, selected);
+                
+                //setSelected((prevState)=>([{...selected, kind:clickCategory.kind[0]}]));
+                //setSelected((prevState)=>{});
+                console.log("clickCategory: ", clickCategory, "selected: ", selected);
               }}/>
               <label htmlFor={child.english}>{child.korean}</label>
             </div>)
           })}
         </div>)
       })}
-      <p>{ }</p>
+      <hr/>
+      <h3>내가 고른 과일은?</h3>
+      <p>
+      {filtered.map(function(element, index){
+          return (<span key={index}>
+            {`${index}. ${element.name} ` }
+          </span>)
+      })}
+      </p>
   </div>)
 }
